a1c01f2493305b949dd9c57326b5c23f1ae43f25
Renê Milagres <renevictorm@gmail.com>
2019-07-01 15:11:15 -0300

    :sparkles:  :recycle:  Adicionando rotas

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 
-server.use(bodyParser.urlencoded({ extended: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
-server.listen(port, function(){
-    console.log(`BACKEND is running on port ${port}`)
+server.listen(port, function() {
+  console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
