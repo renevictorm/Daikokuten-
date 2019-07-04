ce3f99d69382e50b47711c6201b7f0c3d4acdacc
Renê Milagres <renevictorm@gmail.com>
2019-07-04 13:07:49 -0300

    :sparkles: Criando app.jsx e importando depedencias

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../common/template/dependencies'
+
+import Header from '../common/template/header'
+
+export default props => (
+    <div className='wrapper'>
+           <Header />   
+    </div>
+)
